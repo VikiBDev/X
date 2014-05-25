bd4cc44feceed78aa02db1a70e416f37dc93bed5
VikiBonzo <viki.bonzo@wami.it>
2014-05-25 16:35:55 +0200

    gem_count deprecated

--- parser.rb
+++ parser.rb
@@ -6,18 +6,13 @@ def parseLine(line,keywords)
 	parsed_line["generate"] = Array.new
 
 	current_keyword = ""
-	gem_count = -1
 	last_gem = ""
 
 	line.each do |word|
 		if(keywords.include?(word))
 			current_keyword = word
-			if(current_keyword == "AND")
-				gem_count += 1
-			end
 		elsif word == "including"
 			current_keyword = "AND"
-			gem_count += 1
 		else
 			if current_keyword == "AND"
 				parsed_line["AND"][word] = Array.new
@@ -31,5 +26,5 @@ def parseLine(line,keywords)
 	end
 
 	parsed_line
-	
+
 end
